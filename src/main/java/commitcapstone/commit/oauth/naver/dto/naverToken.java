6a108d99306fe90bc4b31f15eacb7813e9401bbf
wjdansrud <wjdansrud092208@gmail.com>
2025-05-09 21:53:03 +0900

    feat: naver 소셜 로그인 구현 / 토큰 갱신/ 가입후 토큰 발급

---
+++ src/main/java/commitcapstone/commit/oauth/naver/dto/naverToken.java
@@ -0,0 +1,26 @@
+package commitcapstone.commit.oauth.naver.dto;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Data;
+
+@Data
+public class naverToken {
+
+    @JsonProperty("access_token")
+    private String accessToken;
+
+    @JsonProperty("refresh_token")
+    private String refreshToken;
+
+    @JsonProperty("token_type")
+    private String tokenType;
+
+    @JsonProperty("expires_in")
+    private long expiresIn;
+
+    @JsonProperty("error")
+    private String error;
+
+    @JsonProperty("error_description")
+    private String errorDescription;
+}
