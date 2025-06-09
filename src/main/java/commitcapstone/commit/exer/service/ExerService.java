867627c8ca5e43055f0b7b3ba3c125286c5aa1a0
wjdansrud <wjdansrud092208@gmail.com>
2025-06-09 09:12:35 +0900

    fix: test

--- src/main/java/commitcapstone/commit/exer/service/ExerService.java
+++ src/main/java/commitcapstone/commit/exer/service/ExerService.java
@@ -35,7 +35,6 @@ public class ExerService {
     @Value("${config.start_date}")
     private String startDate;
 
-
     public ExerService(UserRepository userRepository, WorkRepository workRepository, PointService pointService, PointRepository pointRepository) {
         this.userRepository = userRepository;
         this.workRepository = workRepository;
