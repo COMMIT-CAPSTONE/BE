150032dd5fe7bedcee960ded14e2ab23cecdfb5b
wjdansrud <wjdansrud092208@gmail.com>
2025-06-20 10:29:06 +0900

    feat: 에러 처리 추가

--- src/main/java/commitcapstone/commit/common/GlobalExceptionHandler.java
+++ src/main/java/commitcapstone/commit/common/GlobalExceptionHandler.java
@@ -3,6 +3,7 @@ package commitcapstone.commit.common;
 import commitcapstone.commit.common.exception.OauthException;
 import commitcapstone.commit.common.response.ErrorResponse;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -10,6 +11,12 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 @RestControllerAdvice
 @Slf4j
 public class GlobalExceptionHandler {
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<ErrorResponse> handleException(Exception ex) {
+        ex.printStackTrace();  // 로그 남기기
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+                .body(new ErrorResponse("서버 에러 발생" + ex.getMessage()));
+    }
     //통합 에러 처리
     @ExceptionHandler(OauthException.class)
     public ResponseEntity<ErrorResponse> handleOauthException(OauthException e) {
